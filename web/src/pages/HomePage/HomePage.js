704351df9f514fb3f78fe64b67d9c28fb80de7bc
Philip Harvey <pnharvey12@gmail.com>
2022-11-12 00:51:04 -0500

    Added Acreage, State, Activity input fields
    finished input fields (front end only)

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,13 +1,51 @@
+import { Form, TextField, Label, Submit, FieldError } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
 import ArticlesCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       <>
         <ArticlesCell />
+        <Form onSubmit={onSubmit}>
+          <Label name="Activity" errorClassName="error">
+            Activity
+          </Label>
+          <TextField
+            name="Activity"
+            errorClassName="error"
+            validation={{ required: true }}
+          />
+          <FieldError name="Activity" className="error" />
+
+          <Label name="State" errorClassName="error">
+            State
+          </Label>
+          <TextField
+            name="State"
+            errorClassName="error"
+            validation={{ required: true }}
+          />
+          <FieldError name="State" className="error" />
+
+          <Label name="Acreage" errorClassName="error">
+            Acreage
+          </Label>
+          <TextField
+            name="Acreage"
+            errorClassName="error"
+            validation={{ required: true, pattern: { value: /^[1-9]\d*$/ } }}
+          />
+          <FieldError name="Acreage" className="error" />
+
+          <Submit>generate price</Submit>
+        </Form>
       </>
     </>
   )
